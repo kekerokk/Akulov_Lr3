49c524d5c39afea26a4ca02152c934cc0613b673
Visual studio by Akulov <vspc_vs@VSPC34.RU>
2021-04-05 12:08:16 +0400

    Добавьте файлы проекта.

---
+++ Akulov_Lr3/Akulov_Lr3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    int spisok[10];
+    for (int i = 0; i < size(spisok); i++)
+    {
+        spisok[i] = rand() % 21 - 10;
+    }
+    vector<int> minus;
+    vector<int> plus;
+    for (int i = 0; i < size(spisok); i++)
+    {
+        if (spisok[i] < 0)
+        {
+            minus.push_back(spisok[i]);
+        }
+        else
+            plus.push_back(spisok[i]);
+    }
+    vector<int>::iterator Iter;
+    cout << "Отрицательные:\n";
+    for (Iter = minus.begin(); Iter != minus.end(); Iter++)
+    {
+        cout << *Iter<<endl;
+    }
+    cout << "\nПоложительные:\n";
+    for (Iter = plus.begin(); Iter != plus.end(); Iter++)
+    {
+        cout << *Iter << endl;
+    }
+}
+
